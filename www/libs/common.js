a9b5644781befcf71d016637e814c0394a4b8ced
jiaqiang <1422788179@qq.com>
2017-10-10 18:56:27 +0800

    Initial commit

---
+++ www/libs/common.js
@@ -0,0 +1,65 @@
+var docEl = document.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+        //设置根字体大小
+        var myfontSize = 20 * (docEl.clientWidth / 375);
+        if (myfontSize > 30) {
+            docEl.style.fontSize = "30px";
+        } else {
+            docEl.style.fontSize = 20 * (docEl.clientWidth / 375) + 'px';
+        }
+
+    };
+//绑定浏览器缩放与加载时间
+window.addEventListener(resizeEvt, recalc, false);
+document.addEventListener('DOMContentLoaded', recalc, false);
+document.body.addEventListener('touchstart', function () {
+});
+
+(function ($) {
+    $.fn.alert = function () {
+        return this.fadeIn(500).addClass('animation-show').removeClass('animation-hide')
+    };
+    $.fn.leave = function () {
+        return this.addClass('animation-hide').removeClass('animation-show').fadeOut(500)
+    };
+    $.extend({
+        toast: (a) => {
+            $('.tips').slideDown(200).text(a);
+            setTimeout(() => {
+                $('.tips').slideUp(600)
+            }, 1000)
+        }
+    }
+    );
+
+
+})(jQuery);
+/*底部Tab*/
+
+$('.tab ul li').on('click', function () {
+    const ID = $(this).index()
+    switch (ID) {
+        case 0:
+            location.href = 'index.html';
+            break;
+        case 1:
+            location.href = 'holdPosition.html';
+            break;
+        case 2:
+            location.href = 'master.html';
+            break;
+    }
+})
+
+function toDOU(n) {
+    return n < 10 ? '0' + n : '' + n
+}
+
+function time2date(timestamp) {
+    var oDate = new Date();
+    oDate.setTime(timestamp * 1000)
+    return oDate.getFullYear() + '-' + toDOU(oDate.getMonth() + 1) + '-' + toDOU(oDate.getDate()) + ' ' + toDOU(oDate.getHours()) + ':' + toDOU(oDate.getMinutes())
+        + ':' + toDOU(oDate.getSeconds())
+
+}
